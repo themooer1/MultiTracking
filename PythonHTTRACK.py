a296109f04cd0841ee38fce5f40620cf35474c30
Catalyze326 <cfmcool@gmail.com>
2017-01-31 15:50:27 -0500

    Update PythonHTTRACK.py

--- PythonHTTRACK.py
+++ PythonHTTRACK.py
@@ -20,7 +20,7 @@ def httrack(link):
     global proc
     print(str(proc))
     print("Running HTTRACK")
-    subprocess.Popen(shlex.split("httrack {} -EN 300 -%t .css+.js -O {} --disable-security-limits -AN 99999999999".format(link,'output/'+link)))
+    subprocess.Popen(shlex.split("httrack {} -r3 -M2000000 -c16 *c8 -%s -N '%t' .css+.js -O {} --disable-security-limits -AN 99999999999".format(link,'output/'+link)))
     proc-=1
 proc=0
 for i in links:
